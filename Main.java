5ba9f5e2cfe3d19b41ef4a153e86bca5d2385a8e
Long-pham2008 <phamhoanglong180108@gmail.com>
2025-09-30 18:53:52 +0000

    Hello

--- Main.java
+++ Main.java
@@ -1,28 +1,40 @@
-public class Main
+public class Main 
 {
   public static void main(String[] args)
   {
-    // write your code here
+    Scanner scanner = new Scanner(System.in);
+
+  System.out.print("Enter first string for lastFirstN: ");
+  String s1 = scanner.nextLine();
+  System.out.print("Enter second string for lastFirstN: ");
+  String s2 = scanner.nextLine();
+  System.out.print("enter n: ");
+  int n = scanner.nextInt();
+  scanner.nextLine();
+  lastFirstN(s1, s2, n);
+
+  System.out.print("Enter first string for strinManip:");
+  s1 = scanner.nextInt();
+  System.out.print("Enter second string for strinManip:");
+  s2 = scanner.nextInt();
+  stringManip(s1, s2);
+
   }
 
   /** Precondition: s1 and s2 are not null
-  * Precondition: 0 <= n <= min(s1.length(), s2.length()
+  * Precondition: 0 <= n <= min(s1,length(), s2.length())
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String lastN = s1.substring(s1.length() - n);
+    String firstN = s2.substring(0, n);
+    String output - lastN + firstN;
+
+    System.out.println(output);
   }
 
   /** Precondition: s1 and s2 are not null
-  */
-  public static void stringManip(String s1, String s2)
-  {
-  
-  }
-
-  /** Precondition: s1 and s2 are not null
-  * Precondition: s2 is a substring of s1 and can be found at least one time in s1
-  */
+   */
   public static String removeStr(String s1, String s2)
   {
     String output = null;
